afdb4cd6d3a5891eb191cf3378fcaf2f265dd3ee
justin.keville@gmail.com <justin.keville@gmail.com@860d83d9-13d0-aa78-daaa-60343e7d914b>
2014-01-07 07:45:49 +0000

    initial dump

---
+++ Assets/Scripts/gorilla.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gorilla : MonoBehaviour {
+	public Rigidbody2D barrel;
+	Animator animator;
+	bool ready = true;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("Blend Tree"))
+		{
+			if(ready) 
+			{
+				(Instantiate(barrel, transform.position + new Vector3(-transform.localScale.x*1.6f,transform.localScale.y*1.4f,0), Quaternion.Euler(Vector3.zero)) as Rigidbody2D).velocity = new Vector2(-10f,0);
+				ready = false;
+			}
+		}
+		else 
+		{
+			ready = true;
+		}
+	}
+}
---
+++ Assets/Scripts/shoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class shoot : MonoBehaviour {
+
+	public GameObject beam;
+	float speed = 40f;
+	bool ready = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && ready)
+		{
+			((GameObject) Instantiate(beam, transform.position + new Vector3(transform.localScale.x*1.3f,0,0), Quaternion.Euler(0,0,0))).rigidbody2D.velocity = new Vector2(speed, 0);
+			ready = false;
+			StartCoroutine("ShootCD");
+
+		}
+	}
+
+	IEnumerator ShootCD() 
+	{
+		yield return new WaitForSeconds(.2f);
+		ready = true;
+	}
+}
