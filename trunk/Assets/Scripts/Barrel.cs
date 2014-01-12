82a53319ef5e229c489731154491d12946325c28
justin.keville@gmail.com <justin.keville@gmail.com@860d83d9-13d0-aa78-daaa-60343e7d914b>
2014-01-12 01:02:51 +0000

    now i'm adding the scripts with correct caps back in

---
+++ trunk/Assets/Scripts/Barrel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddTorque (10f);
+	}
+}
---
+++ trunk/Assets/Scripts/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector2 (0, Input.GetAxis ("Vertical") * speed);
+	}
+}
