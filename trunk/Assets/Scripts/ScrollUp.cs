4b796c02a6c1e200b0038b96795f7703ba9a5f39
justin.keville@gmail.com <justin.keville@gmail.com@860d83d9-13d0-aa78-daaa-60343e7d914b>
2014-05-11 05:04:14 +0000

    Credits stop rolling when THANKS FOR PLAYING is in the center

--- trunk/Assets/Scripts/ScrollUp.cs
+++ trunk/Assets/Scripts/ScrollUp.cs
@@ -4,8 +4,15 @@ using System.Collections;
 public class ScrollUp : MonoBehaviour {
 
 	public float scrollSpeed;
+	public float maxHeight;
 
 	public void FulfillDestiny() {
 		rigidbody2D.velocity = new Vector2(0f,scrollSpeed);
+	}
+
+	void Update() {
+		if(transform.position.y > maxHeight) {
+			rigidbody2D.velocity = Vector2.zero;
+		}
 	}
 }
