776517fda5bbd906b5da1b3322aff2b10305854f
justin.keville@gmail.com <justin.keville@gmail.com@860d83d9-13d0-aa78-daaa-60343e7d914b>
2014-08-14 02:35:46 +0000

    Made the gorilla flash red on hit increased life to 50 fixed bug where beam would rotate quickly before destructing on hit improved barrel throwing AI!!!!

--- Assets/Scripts/Gorilla.cs
+++ Assets/Scripts/Gorilla.cs
@@ -7,26 +7,38 @@ public class Gorilla : MonoBehaviour {
 	Animator animator;
 	bool ready = true;
 	int speed = 5;
-	int health = 10;
+	int health = 50;
 	public Texture youWinTexture;
 	public AudioClip gorillaSound;
 	public AudioClip pulloutSound;
+	SpriteRenderer spriteRenderer;
+	Color flashOnHitColor = Color.red;
+	float flashDuration = .1f;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 
 	// Gives gorilla health, destroy gorilla if health equal 0
 	void OnCollisionEnter2D (Collision2D other){
 		if(other.gameObject.tag.Equals("Beam")){
 			health = health - 1;
+			StartCoroutine(FlashOnHit());
 			Destroy (other.gameObject);
 			if (health == 0) {
 				Die();
-				}
 			}
 		}
+	}
+
+	IEnumerator FlashOnHit()
+	{
+		spriteRenderer.color = flashOnHitColor;
+		yield return new WaitForSeconds(flashDuration);
+		spriteRenderer.color = Color.white;
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -34,8 +46,17 @@ public class Gorilla : MonoBehaviour {
 		{
 			if(ready) 
 			{
-				// Generates a value between -3 and 4 (the upper bound of a random range is never hit)
-				var y = Random.Range(-speed+2,speed);
+				int y;
+				if(Random.value > .5) {
+					// Half the time we want to hit this sweet spot
+					// It is the one angle that hits the player when he is in a sweet spot that he can safely shoot all other barrels from
+					y = speed -1;
+				}
+				else
+				{
+					// Generates a value between -3 and 4 (the upper bound of a random range is never hit)
+					y = Random.Range(-speed+2,speed);
+				}
 				Rigidbody2D barrelRB = (Instantiate(barrel, transform.position + new Vector3(-transform.localScale.x*1.6f,transform.localScale.y*1.4f,0), Quaternion.Euler(Vector3.zero)) as Rigidbody2D);
 				barrelRB.velocity = new Vector2(-Mathf.Sqrt(speed * speed - y * y),y);
 				barrelRB.gameObject.transform.parent = spawnedObjects;
